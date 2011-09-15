743190af8ee85da21236d5c2bd9e6b533e79c48d
Steve Phillips <steve@stevephillips.me>
2011-09-14 23:38:42 -0700

    tagged all of the areas that access / use weapon data

--- wpilots.js
+++ wpilots.js
@@ -208,7 +208,7 @@ function start_gameserver(maps, options, shared) {
       no_players:       world.no_players,
       no_ready_players: world.no_ready_players,
       rules:            world.rules
-    }
+    };
   }
 
   /**
@@ -275,7 +275,7 @@ function start_gameserver(maps, options, shared) {
   }
 
   world.on_player_fire = function(player, angle, pos, vel, powerup) {
-   broadcast(OP_PLAYER_FIRE, player.id, angle, pos, vel, powerup);
+   broadcast(OP_PLAYER_FIRE, player.id, angle, pos, vel, powerup); // WEAPON
   }
 
   world.on_player_leave = function(player, reason) {
